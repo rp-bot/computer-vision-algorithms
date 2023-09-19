ff9fc4690b321afb23bdb06237dfe7eb855b62ff
rpbot_mac <71392285+rp-bot@users.noreply.github.com>
2023-09-19 11:28:59 -0400

    lkjbujl

--- img_splitter.cpp
+++ img_splitter.cpp
@@ -8,10 +8,10 @@ int main() {
     const int HEIGHT = 3417;  // Change to your desired height
 
     // Create a 2D vector to hold the data
-    std::vector<std::vector<unsigned char>> data(HEIGHT, std::vector<unsigned char>(WIDTH));
+    std::vector<std::vector<unsigned char> > data(HEIGHT, std::vector<unsigned char>(WIDTH));
 
     // Open the RAW file for reading
-    FILE* inputFile = fopen("0001.raw", "rb");
+    FILE* inputFile = fopen("0002.raw", "rb");
     if (!inputFile) {
         std::cerr << "Error opening input file." << std::endl;
         return 1;
